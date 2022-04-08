c563b8f30d0be7a5c1e5f39193dcdb746d741fdf
AAZWD <azumrawi@gmail.com>
2022-04-08 11:44:26 -0700

    spacing fix

--- src/App.tsx
+++ src/App.tsx
@@ -247,8 +247,8 @@ function App() {
         setIsStatsModalOpen={setIsStatsModalOpen}
         setIsSettingsModalOpen={setIsSettingsModalOpen}
       />
-      <div className="pt-2 px-1 pb-8 md:max-w-7xl w-full mx-auto sm:px-6 lg:px-8 flex flex-col">
-        <div className="pb-6 grow">
+      <div className="pt-2 px-1 pb-8 md:max-w-7xl w-full mx-auto sm:px-6 lg:px-8 flex flex-col grow">
+        <div className="pb-6 mb-8 mt-8">
           <Grid
             guesses={guesses}
             currentGuess={currentGuess}
